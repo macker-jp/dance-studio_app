d45444dedd160dc2e920248f3058ff19b576e4b8
Masaya Fujiki <masaya.fujiki@icloud.com>
2025-04-01 17:46:38 +0900

    header作成

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const ejsMate = require('ejs-mate');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
