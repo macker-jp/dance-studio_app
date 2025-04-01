e98e51c1ae83a5932b2890bfd22a0407c6f6d88e
Masaya Fujiki <masaya.fujiki@icloud.com>
2025-04-01 12:48:43 +0900

    ejsの設定完了

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const path = require('path');
+const ejsMate = require('ejs-mate');
+
 
 const app = express();
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 mongoose.connect('mongodb://127.0.0.1:27017/danceStudio',
     {
@@ -17,6 +24,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/danceStudio',
         console.log('エラーだよ！！');
         console.log(e);
     })
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
 app.listen(4000, () => {
     console.log(`ポート4000でリクエスト待受中...`);
